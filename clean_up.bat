4bf88276f0644b26c9d6192618e01cf3aa3164a9
Richard Mensah <richarda.mensah@gmail.com>
2020-12-09 08:15:38 +0000

    Modified PostgreSQL Conf File

--- clean_up.bat
+++ clean_up.bat
@@ -8,3 +8,5 @@ forfiles -p "db\db_dirs\adbs\bin\log_files\adt_trail" -s -m *.rho -c "cmd /c del
 forfiles -p "db\db_dirs\adbs\bin\log_files\prcs_logs" -s -m *.rho -c "cmd /c del @path"
 forfiles -p "db\db_dirs\adbs\bin" -s -m *.sh -c "cmd /c del @path"
 forfiles -p "db\db_dirs\adbs\Logs" -s -m *.txt -c "cmd /c del @path"
+REM find src/ -size 0 -type f -exec rm -f {} \;
+
